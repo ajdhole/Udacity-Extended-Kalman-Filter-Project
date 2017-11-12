5a741b831ffd08630d32fe12e0d7264c7338cfb1
Anil Dhole <ajdhole@gmail.com>
2017-11-12 14:13:51 +0530

    Final commit

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -22,6 +22,10 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+    x_ = F_ * x_;
+    MatrixXd Ft = F_.transpose();
+    P_ = F_ * P_ * Ft + Q_;
+    
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -29,6 +33,19 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+    VectorXd z_pred = H_ * x_;
+    VectorXd y = z - z_pred;
+    MatrixXd Ht = H_.transpose();
+    MatrixXd S = H_ * P_ * Ht + R_;
+    MatrixXd Si = S.inverse();
+    MatrixXd PHt = P_ * Ht;
+    MatrixXd K = PHt * Si;
+    
+    //new estimate
+    x_ = x_ + (K * y);
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -36,4 +53,36 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+    float px = x_(0);
+    float py = x_(1);
+    float vx = x_(2);
+    float vy = x_(3);
+    
+    // Equations for h_func below
+    float c1 = sqrt(px * px + py * py);
+    //check division by zero
+    if(c1 < .00001) {
+        px += .001;
+        py += .001;
+        c1 = sqrt(px * px + py * py);
+    }
+    float c2 = atan2(py,px);
+    float c3 = (px*vx+py*vy)/c1;
+    
+    //Feed in equations above
+    VectorXd H_func(3);
+    H_func << c1, c2, c3;
+    
+    VectorXd y = z - H_func;
+    MatrixXd Ht = H_.transpose();
+    MatrixXd S = H_ * P_ * Ht + R_;
+    MatrixXd Si = S.inverse();
+    MatrixXd PHt = P_ * Ht;
+    MatrixXd K = PHt * Si;
+    
+    //new estimate
+    x_ = x_ + (K * y);
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * H_) * P_;
 }
